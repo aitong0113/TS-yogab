5cb627a69499a8e9d2bb3f5db437991f941b86a3
Abbie <aitong0113@gmail.com>
2025-12-03 21:19:24 +0800

    page白畫面怎麼修啊啊

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,14 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        dir: resolve(__dirname, 'dist'),
+      },
+    },
   },
   css: {
     preprocessorOptions: {
